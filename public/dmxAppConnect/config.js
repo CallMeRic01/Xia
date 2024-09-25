5ab19817878581cf57cf000d049503240d9596c1
Wappler <git@wappler.io>
2024-09-25 14:41:42 +0800

    update

---
+++ public/dmxAppConnect/config.js
@@ -0,0 +1,18 @@
+dmx.config({
+  "navbar": {
+    "clockInStore": [
+      {
+        "type": "number",
+        "name": "user_id"
+      },
+      {
+        "type": "number",
+        "name": "clockInTime"
+      },
+      {
+        "type": "number",
+        "name": "clockOutTime"
+      }
+    ]
+  }
+});
