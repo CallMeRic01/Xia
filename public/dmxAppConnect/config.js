6846c4aefe23830423a759c281123ce8332acb5b
Wappler <git@wappler.io>
2024-09-27 09:38:54 +0800

    update

--- public/dmxAppConnect/config.js
+++ public/dmxAppConnect/config.js
@@ -12,7 +12,38 @@ dmx.config({
       {
         "type": "number",
         "name": "clockOutTime"
+      },
+      {
+        "type": "text",
+        "name": "user_name"
       }
     ]
+  },
+  "admin_order": {
+    "orderdetails": {
+      "meta": [
+        {
+          "type": "number",
+          "name": "ord_id"
+        },
+        {
+          "type": "number",
+          "name": "pro_id"
+        },
+        {
+          "type": "text",
+          "name": "pro_name"
+        },
+        {
+          "type": "number",
+          "name": "pro_price"
+        },
+        {
+          "type": "number",
+          "name": "detOrd_quantity"
+        }
+      ],
+      "outputType": "array"
+    }
   }
 });
